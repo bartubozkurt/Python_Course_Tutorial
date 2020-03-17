f07e13f651198617dddb433066c62cf4d082101f
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2020-03-17 23:48:34 +0300

    Add files via upload

---
+++ Workshops/KacMileEder.py
@@ -0,0 +1,6 @@
+degisken1 = int(input("Km Giriniz?"))
+kmToMile = 0.621371192
+
+method = degisken1 * kmToMile
+
+print(str(degisken1) +"Km = " + str(method) + " mil eder!")
---
+++ Workshops/sayilarinYeriniDegistir.py
@@ -0,0 +1,13 @@
+x = 10
+y = 20
+
+temp = x
+x = y     # yer değiştirme
+y = temp
+
+x,y = y,x # yer degiştirme python bu sekilde de kullanılır!
+
+print("x = " + str(x))
+print("y = " + str(y))
+
+
