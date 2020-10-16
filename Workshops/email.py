e0314a7e7dc3692c9a9e67081d0e6bb19981ca88
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2020-10-16 18:21:31 +0300

    Create email.py

---
+++ Workshops/email.py
@@ -0,0 +1,8 @@
+def full_emails(people):
+  result = []
+  for  name, email in people:
+    result.append(("{} <{}>".format(name,email))
+   return result
+
+print(full_emails(["bartubozkurt35@gmail.com", "Bartu Bozkurt",
+      ("altaybzkrt@hotmail.com", "Altay Bozkurt")]))
